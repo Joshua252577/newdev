20b0b2deb39bff10c436247620d22096e2a4a828
unknown <99177444matheus@gmail.com>
2022-05-26 19:12:30 -0300

    atualização do J.S 26/05/2022

--- web/aula-1/listas/main.js
+++ web/aula-1/listas/main.js
@@ -1,9 +1,11 @@
-const buttonSendMessage = document.getElementById('buttonSendMessage');
+const buttonSendMessage = 
+  document.getElementById('buttonSendMessage');
 
 const messages = [];
 
 function addMessage(event) {
   event.preventDefault(); // Desabilita o evento padrão do elemento
+
   const inputName = document.getElementById('name');
   const inputemail = document.getElementById('email');
   const inputmessage = document.getElementById('messages');
@@ -13,8 +15,21 @@ function addMessage(event) {
     email: inputemail.value,
     message: inputmessage.value
   }
-  document.getElementById('form-messages').reset();
-  console.log('...', message);
 
-  buttonSendMessage.addEventListener('click',addMessageToMessages);
+    if (message.length < 3) {
+      massages.push(messages);
+    }else {
+      //Chama  o alert do browser
+      alert('o vetor está cheio');
+    } 
+
+  //epegando o formulario e utilizando um metodo do form que é o       
+  document.getElementById('form-messages').reset();
+  console.log('Imprimir vetor', messages);
+
 }
+
+buttonSendMessage.addEventListener('click', addMessages);
+
+
+
