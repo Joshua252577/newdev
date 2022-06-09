f270587ca8c765a4183887a52a5f5b25c411216d
Matheus Joshua <99177444matheus@gmail.com>
2022-06-08 21:17:17 -0300

    enviando-arquivos

--- web/web-app/projeto-desenvolvimento-web1/cadastro.js
+++ web/web-app/projeto-desenvolvimento-web1/cadastro.js
@@ -1,25 +1,89 @@
 let peoples = [];
 
+let identificadorQueTaSendoEditado = null;
+
 const loadPeoples = () => {
   const itemsJaArmazenados = localStorage.getItem('listaDePessoas');
   return itemsJaArmazenados ? JSON.parse(itemsJaArmazenados) : [];
 }
-const span = () => {
-      const span = document.createElement('span')
-      const iconEdit = document.createElement('i')
-      iconEdit.setAttribute('class', 'fas fa-edit');
-      iconEdit.setAttribute('title', 'Editar');
-      iconEdit.setAttribute('style', 'cursor:pointer; margin-inline: 1rem;');
-      span.appendChild(iconEdit);
 
-      const iconRemove = document.createElement('i');
-      iconRemove.setAttribute('class', 'fas fa-trash');
-      iconRemove.setAttribute('title', 'Remover');
-      iconRemove.setAttribute('style', 'cursor:pointer; margin-inline: 1rem;');
-      span.appendChild(iconRemove);
+const onClickEdit = (element) => {
+  const identificadorASerEncontrado = 
+    element.getAttribute('identificador');
 
-      return span;
+  identificadorQueTaSendoEditado = +identificadorASerEncontrado;
+  
+  const peoples = loadPeoples();
+  console.log('carregar pessoas', peoples);
+  let pessoaEncontrada = {
+    name: '',
+    age: '',
+    height: ''
+  };
+
+  peoples.forEach((pessoa, identificador) => {
+    if (identificador === +identificadorASerEncontrado) {
+      pessoaEncontrada.age = pessoa.age;
+      pessoaEncontrada.name = pessoa.name;
+      pessoaEncontrada.height = pessoa.height;
+    }
+  });
+
+  document.getElementById('name').value = pessoaEncontrada.name;
+  document.getElementById('age').value = pessoaEncontrada.age;
+  document.getElementById('height').value = pessoaEncontrada.height;
+
+  console.log('pessoaEncontrada', pessoaEncontrada);
 }
+
+const onClickRemove = (element) => {
+  console.log('on click delete', element.getAttribute('identificador'));
+}
+
+const salvarRegistroEditado = (registroSendoEditado) => {
+  const pessoas = loadPeoples();
+  const pessoasAtualizadas = pessoas.map((pessoa, index) => { 
+    if (identificadorQueTaSendoEditado === index) {
+      pessoa.name = registroSendoEditado.name;
+      pessoa.age = registroSendoEditado.age;
+      pessoa.height = registroSendoEditado.height;
+    }
+    return pessoa;
+  })
+
+  localStorage.setItem('listaDePessoas', JSON.stringify(pessoasAtualizadas));
+
+  identificadorQueTaSendoEditado = null;
+
+  listPeoples();
+  document.querySelector('form').reset();
+}
+
+
+const span = (identificador) => {
+  const span = document.createElement('span');
+  const iconEdit = document.createElement('i');
+  iconEdit.setAttribute('class', 'fas fa-edit');
+  iconEdit.setAttribute('title', 'Editar');
+
+  iconEdit.setAttribute('identificador', `${identificador}`);
+  iconEdit.setAttribute('onclick', `onClickEdit(this)`);
+  iconEdit.setAttribute('style', 'cursor:pointer; margin-inline: 1rem;');
+
+  span.appendChild(iconEdit);
+
+  const iconRemove = document.createElement('i');
+  iconRemove.setAttribute('class', 'fas fa-trash');
+  iconRemove.setAttribute('title', 'Remover');
+  iconRemove.setAttribute('identificador', `${identificador}`);
+  iconRemove.setAttribute('onclick', `onClickRemove(this)`);
+  iconRemove.setAttribute('style', 'cursor:pointer; margin-inline: 1rem;');
+
+  span.appendChild(iconRemove);
+  
+  return span;
+} 
+
 const listPeoples = () => {
   const peoples = loadPeoples();
   
@@ -30,14 +94,19 @@ const listPeoples = () => {
   
   ul = document.createElement('ul');
  
-  peoples.forEach((item, index) => {
+  peoples.forEach((item, identificador) => {
     const li = document.createElement('li');
-    li.innerHTML = `Nome: ${item.name} Idade: ${item.age}, Altura: ${item.height}`
-    console.log('ITEMS', item)
+    li.innerHTML = 
+    
+    `Nome: ${item.name} 
+     Idade: ${item.age}, 
+     Altura: ${item.height}
+     `;
+
+    li.appendChild(span(identificador))
     ul.appendChild(li);
   });
-
-
+  
   document.getElementById('list-section').appendChild(ul);
 }
 
@@ -45,14 +114,21 @@ listPeoples();
 
 const addPeople = (event) => {
   event.preventDefault();
+
   const people = {
     name: document.getElementById('name').value,
     age: document.getElementById('age').value,
     height: document.getElementById('height').value,
   }
+  console.log('after save registry', identificadorQueTaSendoEditado);
+  if (identificadorQueTaSendoEditado || identificadorQueTaSendoEditado === 0) {
+    salvarRegistroEditado(people);
+    return;
+  }
   
   peoples = loadPeoples();
 
+  console.log('after save registry');
   peoples.push(people);
   
   localStorage.setItem('listaDePessoas', JSON.stringify(peoples));
