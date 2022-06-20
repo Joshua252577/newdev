95e41cc1a5af5e96a6293c1c1cb8304adef92e67
Matheus Joshua <99177444matheus@gmail.com>
2022-06-20 20:08:05 -0300

    conclusão do projeto concessionária

--- web/web-app/projeto-garagem-curso/cadastro.js
+++ web/web-app/projeto-garagem-curso/cadastro.js
@@ -1,124 +1,153 @@
-let carros = []
+let carros = [];
 
-let identificadorQueTaSendoEditado = null
+let identificadorQueTaSendoEditado = null;
 
-const carregarCarros = () => {
-  const itemsJaArmazenados = localStorage.getItem('listaDeCarros')
-  return itemsJaArmazenados ? JSON.parse(itemsJaArmazenados) : []
+const loadCarros = () => {
+  const itemsJaArmazenados = localStorage.getItem('listaDeCarros');
+  return itemsJaArmazenados ? JSON.parse(itemsJaArmazenados) : [];
 }
 
-const onClickEdit = element => {
-  const identificadorASerEncontrado = element.getAttribute(' identificador')
+const onClickEdit = (element) => {
+  const identificadorASerEncontrado = 
+    element.getAttribute('identificador');
 
-  identificadorQueTaSendoEditado = +identificadorASerEncontrado
-
-  const carros = carregarCarros()
-  console.log('carregar carros', carros)
-  let carrosEncontrados = {
+  identificadorQueTaSendoEditado = +identificadorASerEncontrado;
+  
+  const carros = loadCarros();
+  console.log('carregar carros', carros);
+  let carroEncontrado = {
     marca: '',
     modelo: '',
     ano: ''
-  }
+  };
 
-  carros.forEach((item, identificador) => {
+  carros.forEach((car, identificador) => {
     if (identificador === +identificadorASerEncontrado) {
-      carroEncontrado.marca = item.marca
-      carroEncontrado.modelo = item.modelo
-      carroEncontrado.ano = item.ano
+      carroEncontrado.modelo = car.modelo;
+      carroEncontrado.marca = car.marca;
+      carroEncontrado.ano = car.ano;
     }
-  })
+  });
 
-  document.getElementById('marca').value = carrosEncontrado.marca
-  document.getElementById('modelo').value = carroEncontrado.modelo
-  document.getElementById('ano').value = carroEncontrado.ano
+  document.getElementById('marca').value = carroEncontrado.marca;
+  document.getElementById('modelo').value = carroEncontrado.modelo;
+  document.getElementById('ano').value = carroEncontrado.ano;
 
-  console.log('carro encontrado', carroEncontrado)
+  console.log('carroEncontrado', carroEncontrado);
 }
 
-const salvarRegistroEditado = regisdtroSendoEditado => {  
-  const cars = carregarCarros()
-  const carrosAtualizados = cars.map((car, index) => {
+const onClickRemove = (element) => {
+  const identificadorASerEncontrado = 
+  +element.getAttribute('identificador');
+
+  const cars = loadCarros();
+
+  cars.splice(identificadorASerEncontrado, 1);
+  
+  localStorage.setItem('listaDeCarros', JSON.stringify(cars));
+  listCarros();
+}
+
+const salvarRegistroEditado = (registroSendoEditado) => {
+  const cars = loadCarros();
+  const carrosAtualizados = cars.map((car, index) => { 
     if (identificadorQueTaSendoEditado === index) {
-      car.marca = regisdtroSendoEditado.marca
-      car.modelo = regisdtroSendoEditado.modelo
-      car.ano = regisdtroSendoEditado.ano
+      car.name = registroSendoEditado.marca;
+      car.age = registroSendoEditado.modelo;
+      car.height = registroSendoEditado.ano;
     }
-    return car
+    return car;
   })
 
-  localStorage.setItem('listaDeCarros', JSON.stringify(carrosAtualizados))
+  localStorage.setItem('listaDeCarros', JSON.stringify(carrosAtualizados));
 
-  identificadorQueTaSendoEditado = null
+  identificadorQueTaSendoEditado = null;
 
-  listaDeCarros()
-  document.querySelector('form').reset()
+  listCarros();
+  document.querySelector('form').reset();
 }
 
-const span = identificador => {
-  const span = document.createElement('span')
-  const iconEdit = document.createElement('i')
-  iconEdit.setAttribute('class', 'fas fa-edit')
-  iconEdit.setAttribute('title', 'Editar')
 
-  iconEdit.setAttribute('identificador', `${identificador}`)
-  iconEdit.setAttribute('onclick', `onClickEdit(this)`)
-  iconEdit.setAttribute('style', 'cursor:pointer; margin-inline: 1rem;')
+const span = (identificador) => {
+  const span = document.createElement('span');
+  const iconEdit = document.createElement('i');
+  iconEdit.setAttribute('class', 'fas fa-edit');
+  iconEdit.setAttribute('title', 'Editar');
 
-  span.appendChild(iconEdit)
+  iconEdit.setAttribute('identificador', `${identificador}`);
+  iconEdit.setAttribute('onclick', `onClickEdit(this)`);
+  iconEdit.setAttribute('style', 'cursor:pointer; margin-inline: 1rem;');
 
-  return span
-}
+  span.appendChild(iconEdit);
 
-const listaDeCarros = () => {
-  const carros = carregarCarros()
+  const iconRemove = document.createElement('i');
+  iconRemove.setAttribute('class', 'fas fa-trash');
+  iconRemove.setAttribute('title', 'Remover');
+  iconRemove.setAttribute('identificador', `${identificador}`);
+  iconRemove.setAttribute('onclick', `onClickRemove(this)`);
+  iconRemove.setAttribute('style', 'cursor:pointer; margin-inline: 1rem;');
 
-  let ul = document.querySelector('ul')
+  span.appendChild(iconRemove);
+  
+  return span;
+} 
+
+const listCarros = () => {
+  const carros = loadCarros();
+  
+  let ul = document.querySelector('ul');
   if (ul) {
-    ul.remove()
+    ul.remove();
   }
-
-  ul = document.createElement('ul')
-
+  
+  ul = document.createElement('ul');
+ 
   carros.forEach((item, identificador) => {
-    const li = document.createElement('li')
-    li.innerHTML = `Marca: ${item.marca} 
-      Modelo: ${item.modelo}, 
-      Ano: ${item.ano}`
+    const li = document.createElement('li');
+    li.innerHTML = 
+    
+    `Marca: ${item.marca} 
+     Modelo: ${item.modelo}, 
+     Ano: ${item.ano}
+     `;
 
     li.appendChild(span(identificador))
-    ul.appendChild(li)
-  })
-
-  document.getElementById('list-section').appendChild(ul)
+    ul.appendChild(li);
+  });
+  
+  document.getElementById('list-section').appendChild(ul);
 }
 
-listaDeCarros()
+listCarros();
 
-const adicionarCarro = event => {
-  event.preventDefault()
+const addCarro = (event) => {
+  event.preventDefault();
 
   const carro = {
     marca: document.getElementById('marca').value,
     modelo: document.getElementById('modelo').value,
-    ano: document.getElementById('ano').value
+    ano: document.getElementById('ano').value,
   }
-  console.log('proximo valor a ser registrado', identificadorQueTaSendoEditado)
+  console.log('after save registry', identificadorQueTaSendoEditado);
   if (identificadorQueTaSendoEditado || identificadorQueTaSendoEditado === 0) {
-    salvarRegistroEditado(carro)
-    return
+    salvarRegistroEditado(carro);
+    return;
   }
+  
+  carros = loadCarros();
 
-  carros = carregarCarros()
+  console.log('after save registry');
+  carros.push(carro);
+  
+  localStorage.setItem('listaDeCarros', JSON.stringify(carros));
 
-  console.log('proximo carro registrado')
-  carros.push(carro)
+  document.querySelector('form').reset();
 
-  localStorage.setItem('listaDeCarros', JSON.stringify(carros))
-
-  document.querySelector('form').reset()
-
-  listaDeCarros()
+   listCarros();
 }
 
-const botaoDeAdicionar = document.getElementById('btn')
-botaoDeAdicionar.addEventListener('click', adicionarCarro)
+
+
+
+const botaoDeAdicionar = document.getElementById('btn');
+botaoDeAdicionar.addEventListener('click', addCarro)
