72a9875c39957c9302121125b0754f8e33f47243
Matheus Joshua <99177444matheus@gmail.com>
2022-08-01 21:22:12 -0300

    incremento dos videos na aplicação

--- web/angular/education-front/src/app/app.module.ts
+++ web/angular/education-front/src/app/app.module.ts
@@ -3,19 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { CourseComponent } from './pages/course/course.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LogoComponent } from './components/logo/logo.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { RoomComponent } from './components/room/room.component';
+import { LessonComponent } from './components/lesson/lesson.component';
+
+import { VimeModule } from '@vime/angular';
 
 @NgModule({
   declarations: [
     AppComponent,
+    CourseComponent,
     HeaderComponent,
     LogoComponent,
-    LessonComponent,
     SidebarComponent,
     RoomComponent,
-    CourseComponent
+    LessonComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    VimeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- web/angular/education-front/src/app/components/lesson/lesson.component.ts
+++ web/angular/education-front/src/app/components/lesson/lesson.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-lesson',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./lesson.component.css']
 })
 export class LessonComponent implements OnInit {
+  @Input () lessonid?: number;
+  @Input () title?: string;
 
   constructor() { }
 
